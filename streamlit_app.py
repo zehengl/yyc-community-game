bca6b294d055cfa442dc810854f50871bf418041
Zeheng Li <imzehengl@gmail.com>
2023-12-28 09:43:04 -0700

    clean up

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 
 st.set_page_config(page_title="yyc-community-game", page_icon=":question:")
 _, center, _ = st.columns([2, 1, 2])
